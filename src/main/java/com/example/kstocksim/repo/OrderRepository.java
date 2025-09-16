e31355b1f50189af4fca9a1a6d05a1ced224e457
Fatfamily <alge456789@gmail.com>
2025-09-16 09:31:28 +0900

    Delete src/main directory

--- src/main/java/com/example/kstocksim/repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kstocksim.repo;
-
-import com.example.kstocksim.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserIdOrderByExecutedAtDesc(Long userId);
-}
